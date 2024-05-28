3a85b3ac116a2e97eadc917781b1f5a9bfbbcfe9
feisalmombo <feisalmombo29@gmail.com>
2024-05-28 13:37:25 +0300

    fix bugs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,6 @@
 |
 */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization, X-Requested-With, Content-Type, Accept');
-
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
