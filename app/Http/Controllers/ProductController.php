95b5ae3107a6d1e9b5af263dbffb0c7e3f733533
feisalmombo <feisalmombo29@gmail.com>
2024-05-28 16:14:28 +0300

    fix bugs

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 
 class ProductController extends Controller
@@ -16,23 +15,7 @@ class ProductController extends Controller
     */
     public function index()
     {
-        //return response()->json(Product::all(), 200);
-
-        $products = Product::all();
-
-        if($products->count() > 0){
-
-            return response()->json([
-                'status' => 200,
-                'products' => $products
-            ], 200);
-
-        }else {
-            return response()->json([
-                'status' => 404,
-                'message' => 'No Records Found'
-            ], 404);
-        }
+        return response()->json(Product::all(), 200);
     }
 
 
@@ -41,43 +24,16 @@ class ProductController extends Controller
     */
     public function store(Request $request)
     {
-
-        $validatedData = Validator::make($request->all(), [
-                'name' => 'required|max:255',
-                'description' => 'required',
-                'price' => 'required|numeric',
+    $validatedData = $request->validate([
+        'name' => 'required|max:255',
+        'description' => 'required',
+        'price' => 'required|numeric',
         ]);
 
-        if($validatedData->fails()) {
 
-            return response()->json([
-                'status' => 422,
-                'errors' => $validatedData->messages()
-            ], 422);
+        $product = Product::create($validatedData);
 
-        }else {
-
-            $product = Product::create([
-                'name' => $request->name,
-                'description' => $request->description,
-                'price' => $request->price,
-            ]);
-
-            if($product) {
-
-                return response()->json([
-                    'status' => 200,
-                    'message' => "Product Created Successfully"
-                ], 200);
-
-            }else {
-
-                return response()->json([
-                    'status' => 500,
-                    'message' => "Something Went Wrong"
-                ], 500);
-            }
-        }
+        return response()->json($product, 201);
     }
 
 
@@ -86,85 +42,45 @@ class ProductController extends Controller
     */
     public function show(string $id)
     {
-            $product = Product::find($id);
+        // Retrieve the product from the database using the provided $id
+        $product = Product::find($id);
 
-            if($product) {
-                return response()->json([
-                    'status' => 200,
-                    'product' => $product
-                ], 200);
 
-            }else {
-
-                return response()->json([
-                    'status' => 404,
-                    'message' => "No Such Product Found!"
-                ], 404);
-
-            }
+        // Check if the product was found
+        if ($product) {
+            // Return the product as a JSON response with a 200 HTTP status code
+            return response()->json($product, 200);
+        } else {
+            // Return a 404 Not Found HTTP status code if the product was not found
+        return response()->json(['message' => 'Product not found'], 404);
+        }
     }
 
 
     /**
     * Update the specified resource in storage.
     */
-    public function update(Request $request, int $id)
+    public function update(Request $request, Product $product)
     {
-
-        // $validatedData = $request->validate([
-        //     'name' => 'required|max:255',
-        //     'description' => 'required',
-        //     'price' => 'required|numeric',
-        // ]);
-
-        // if ($product) {
-
-        // $product->update($validatedData);
-
-        // return response()->json($product, 200);
-        // } else {
-
-        // return response()->json(['message' => 'Product not found'], 404);
-        // }
-
-
-        $validattor = Validator::make($request->all(), [
+        // Validate the request data
+        $validatedData = $request->validate([
             'name' => 'required|max:255',
             'description' => 'required',
             'price' => 'required|numeric',
         ]);
 
-        if($validattor->fails()) {
 
-            return response()->json([
-                'status' => 422,
-                'errors' => $validattor->messages()
-            ], 422);
+        // Check if the product was found
+        if ($product) {
+        // Update the product with the validated data
+        $product->update($validatedData);
 
-        }else {
 
-            $product = Product::find($id);
-
-            if($product) {
-
-                $product->update([
-                    'name' => $request->name,
-                    'description' => $request->description,
-                    'price' => $request->price,
-                ]);
-
-                return response()->json([
-                    'status' => 200,
-                    'message' => "Product Updated Successfully"
-                ], 200);
-
-            }else {
-
-                return response()->json([
-                    'status' => 404,
-                    'message' => "No Such Product Found!"
-                ], 404);
-            }
+        // Return the updated product as a JSON response with a 200 HTTP status code
+        return response()->json($product, 200);
+        } else {
+        // Return a 404 Not Found HTTP status code if the product was not found
+        return response()->json(['message' => 'Product not found'], 404);
         }
     }
 
